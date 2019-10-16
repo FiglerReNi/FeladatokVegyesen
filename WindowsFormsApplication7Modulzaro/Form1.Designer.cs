0da1c4dc5344876cdc6bc85502d9985ec649e6da
FiglerReNi <50335163+FiglerReNi@users.noreply.github.com>
2019-10-16 20:57:07 +0200

    forrás

---
+++ WindowsFormsApplication7Modulzaro/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace WindowsFormsApplication7Modulzaro
+{
+    partial class Dolgozok
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Dolgozoklany1 = new System.Windows.Forms.Label();
+            this.Dolgozokfiu1 = new System.Windows.Forms.Label();
+            this.Dolgozoklany2 = new System.Windows.Forms.ComboBox();
+            this.Dolgozokfiu2 = new System.Windows.Forms.ComboBox();
+            this.Dolgozokosszesito = new System.Windows.Forms.GroupBox();
+            this.Osszesito6ev2 = new System.Windows.Forms.TextBox();
+            this.Osszesitokor2 = new System.Windows.Forms.TextBox();
+            this.Osszesitolegidosebb2 = new System.Windows.Forms.TextBox();
+            this.Osszesito6ev1 = new System.Windows.Forms.Label();
+            this.Osszesitokor1 = new System.Windows.Forms.Label();
+            this.Osszesitolegidosebb1 = new System.Windows.Forms.Label();
+            this.Osszesitofiu = new System.Windows.Forms.RadioButton();
+            this.Osszesitolany = new System.Windows.Forms.RadioButton();
+            this.Dolgozokadatok = new System.Windows.Forms.GroupBox();
+            this.Adatokkor2 = new System.Windows.Forms.TextBox();
+            this.Adatokmiota2 = new System.Windows.Forms.TextBox();
+            this.Adatokmiota1 = new System.Windows.Forms.Label();
+            this.Adatokkor1 = new System.Windows.Forms.Label();
+            this.Dolgozikmind2 = new System.Windows.Forms.CheckBox();
+            this.Dolgozikment = new System.Windows.Forms.Button();
+            this.Dolgozokosszesito.SuspendLayout();
+            this.Dolgozokadatok.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Dolgozoklany1
+            // 
+            this.Dolgozoklany1.AutoSize = true;
+            this.Dolgozoklany1.Location = new System.Drawing.Point(57, 48);
+            this.Dolgozoklany1.Name = "Dolgozoklany1";
+            this.Dolgozoklany1.Size = new System.Drawing.Size(48, 13);
+            this.Dolgozoklany1.TabIndex = 0;
+            this.Dolgozoklany1.Text = "Lányok: ";
+            // 
+            // Dolgozokfiu1
+            // 
+            this.Dolgozokfiu1.AutoSize = true;
+            this.Dolgozokfiu1.Location = new System.Drawing.Point(334, 48);
+            this.Dolgozokfiu1.Name = "Dolgozokfiu1";
+            this.Dolgozokfiu1.Size = new System.Drawing.Size(30, 13);
+            this.Dolgozokfiu1.TabIndex = 1;
+            this.Dolgozokfiu1.Text = "Fiúk:";
+            // 
+            // Dolgozoklany2
+            // 
+            this.Dolgozoklany2.FormattingEnabled = true;
+            this.Dolgozoklany2.Location = new System.Drawing.Point(111, 45);
+            this.Dolgozoklany2.Name = "Dolgozoklany2";
+            this.Dolgozoklany2.Size = new System.Drawing.Size(121, 21);
+            this.Dolgozoklany2.TabIndex = 2;
+            this.Dolgozoklany2.SelectedIndexChanged += new System.EventHandler(this.Dolgozoklany2_SelectedIndexChanged);
+            this.Dolgozoklany2.SelectedValueChanged += new System.EventHandler(this.Dolgozoklany2_SelectedValueChanged);
+            // 
+            // Dolgozokfiu2
+            // 
+            this.Dolgozokfiu2.FormattingEnabled = true;
+            this.Dolgozokfiu2.Location = new System.Drawing.Point(370, 45);
+            this.Dolgozokfiu2.Name = "Dolgozokfiu2";
+            this.Dolgozokfiu2.Size = new System.Drawing.Size(121, 21);
+            this.Dolgozokfiu2.TabIndex = 3;
+            this.Dolgozokfiu2.SelectedValueChanged += new System.EventHandler(this.Dolgozokfiu2_SelectedValueChanged);
+            // 
+            // Dolgozokosszesito
+            // 
+            this.Dolgozokosszesito.Controls.Add(this.Osszesito6ev2);
+            this.Dolgozokosszesito.Controls.Add(this.Osszesitokor2);
+            this.Dolgozokosszesito.Controls.Add(this.Osszesitolegidosebb2);
+            this.Dolgozokosszesito.Controls.Add(this.Osszesito6ev1);
+            this.Dolgozokosszesito.Controls.Add(this.Osszesitokor1);
+            this.Dolgozokosszesito.Controls.Add(this.Osszesitolegidosebb1);
+            this.Dolgozokosszesito.Controls.Add(this.Osszesitofiu);
+            this.Dolgozokosszesito.Controls.Add(this.Osszesitolany);
+            this.Dolgozokosszesito.Location = new System.Drawing.Point(60, 110);
+            this.Dolgozokosszesito.Name = "Dolgozokosszesito";
+            this.Dolgozokosszesito.Size = new System.Drawing.Size(200, 201);
+            this.Dolgozokosszesito.TabIndex = 4;
+            this.Dolgozokosszesito.TabStop = false;
+            this.Dolgozokosszesito.Text = "Összesítő";
+            // 
+            // Osszesito6ev2
+            // 
+            this.Osszesito6ev2.Location = new System.Drawing.Point(109, 144);
+            this.Osszesito6ev2.Name = "Osszesito6ev2";
+            this.Osszesito6ev2.Size = new System.Drawing.Size(85, 20);
+            this.Osszesito6ev2.TabIndex = 7;
+            // 
+            // Osszesitokor2
+            // 
+            this.Osszesitokor2.Location = new System.Drawing.Point(109, 110);
+            this.Osszesitokor2.Name = "Osszesitokor2";
+            this.Osszesitokor2.Size = new System.Drawing.Size(85, 20);
+            this.Osszesitokor2.TabIndex = 6;
+            // 
+            // Osszesitolegidosebb2
+            // 
+            this.Osszesitolegidosebb2.Location = new System.Drawing.Point(109, 81);
+            this.Osszesitolegidosebb2.Name = "Osszesitolegidosebb2";
+            this.Osszesitolegidosebb2.Size = new System.Drawing.Size(85, 20);
+            this.Osszesitolegidosebb2.TabIndex = 5;
+            // 
+            // Osszesito6ev1
+            // 
+            this.Osszesito6ev1.AutoSize = true;
+            this.Osszesito6ev1.Location = new System.Drawing.Point(14, 147);
+            this.Osszesito6ev1.Name = "Osszesito6ev1";
+            this.Osszesito6ev1.Size = new System.Drawing.Size(74, 13);
+            this.Osszesito6ev1.TabIndex = 4;
+            this.Osszesito6ev1.Text = "6 éve dolgozó";
+            // 
+            // Osszesitokor1
+            // 
+            this.Osszesitokor1.AutoSize = true;
+            this.Osszesitokor1.Location = new System.Drawing.Point(17, 113);
+            this.Osszesitokor1.Name = "Osszesitokor1";
+            this.Osszesitokor1.Size = new System.Drawing.Size(59, 13);
+            this.Osszesitokor1.TabIndex = 3;
+            this.Osszesitokor1.Text = "Összes kor";
+            // 
+            // Osszesitolegidosebb1
+            // 
+            this.Osszesitolegidosebb1.AutoSize = true;
+            this.Osszesitolegidosebb1.Location = new System.Drawing.Point(17, 84);
+            this.Osszesitolegidosebb1.Name = "Osszesitolegidosebb1";
+            this.Osszesitolegidosebb1.Size = new System.Drawing.Size(62, 13);
+            this.Osszesitolegidosebb1.TabIndex = 2;
+            this.Osszesitolegidosebb1.Text = "Legidősebb";
+            // 
+            // Osszesitofiu
+            // 
+            this.Osszesitofiu.AutoSize = true;
+            this.Osszesitofiu.Location = new System.Drawing.Point(109, 36);
+            this.Osszesitofiu.Name = "Osszesitofiu";
+            this.Osszesitofiu.Size = new System.Drawing.Size(36, 17);
+            this.Osszesitofiu.TabIndex = 1;
+            this.Osszesitofiu.TabStop = true;
+            this.Osszesitofiu.Text = "fiú";
+            this.Osszesitofiu.UseVisualStyleBackColor = true;
+            this.Osszesitofiu.CheckedChanged += new System.EventHandler(this.Osszesitofiu_CheckedChanged);
+            // 
+            // Osszesitolany
+            // 
+            this.Osszesitolany.AutoSize = true;
+            this.Osszesitolany.Location = new System.Drawing.Point(17, 36);
+            this.Osszesitolany.Name = "Osszesitolany";
+            this.Osszesitolany.Size = new System.Drawing.Size(44, 17);
+            this.Osszesitolany.TabIndex = 0;
+            this.Osszesitolany.TabStop = true;
+            this.Osszesitolany.Text = "lány";
+            this.Osszesitolany.UseVisualStyleBackColor = true;
+            this.Osszesitolany.CheckedChanged += new System.EventHandler(this.Osszesitolany_CheckedChanged);
+            // 
+            // Dolgozokadatok
+            // 
+            this.Dolgozokadatok.Controls.Add(this.Adatokkor2);
+            this.Dolgozokadatok.Controls.Add(this.Adatokmiota2);
+            this.Dolgozokadatok.Controls.Add(this.Adatokmiota1);
+            this.Dolgozokadatok.Controls.Add(this.Adatokkor1);
+            this.Dolgozokadatok.Location = new System.Drawing.Point(337, 110);
+            this.Dolgozokadatok.Name = "Dolgozokadatok";
+            this.Dolgozokadatok.Size = new System.Drawing.Size(200, 137);
+            this.Dolgozokadatok.TabIndex = 5;
+            this.Dolgozokadatok.TabStop = false;
+            this.Dolgozokadatok.Text = "Adatok";
+            // 
+            // Adatokkor2
+            // 
+            this.Adatokkor2.Location = new System.Drawing.Point(94, 46);
+            this.Adatokkor2.Name = "Adatokkor2";
+            this.Adatokkor2.Size = new System.Drawing.Size(90, 20);
+            this.Adatokkor2.TabIndex = 3;
+            // 
+            // Adatokmiota2
+            // 
+            this.Adatokmiota2.Location = new System.Drawing.Point(94, 81);
+            this.Adatokmiota2.Name = "Adatokmiota2";
+            this.Adatokmiota2.Size = new System.Drawing.Size(90, 20);
+            this.Adatokmiota2.TabIndex = 2;
+            // 
+            // Adatokmiota1
+            // 
+            this.Adatokmiota1.AutoSize = true;
+            this.Adatokmiota1.Location = new System.Drawing.Point(9, 84);
+            this.Adatokmiota1.Name = "Adatokmiota1";
+            this.Adatokmiota1.Size = new System.Drawing.Size(75, 13);
+            this.Adatokmiota1.TabIndex = 1;
+            this.Adatokmiota1.Text = "Mióta dolgozik";
+            // 
+            // Adatokkor1
+            // 
+            this.Adatokkor1.AutoSize = true;
+            this.Adatokkor1.Location = new System.Drawing.Point(6, 49);
+            this.Adatokkor1.Name = "Adatokkor1";
+            this.Adatokkor1.Size = new System.Drawing.Size(23, 13);
+            this.Adatokkor1.TabIndex = 0;
+            this.Adatokkor1.Text = "Kor";
+            // 
+            // Dolgozikmind2
+            // 
+            this.Dolgozikmind2.AutoSize = true;
+            this.Dolgozikmind2.Location = new System.Drawing.Point(337, 257);
+            this.Dolgozikmind2.Name = "Dolgozikmind2";
+            this.Dolgozikmind2.Size = new System.Drawing.Size(98, 17);
+            this.Dolgozikmind2.TabIndex = 6;
+            this.Dolgozikmind2.Text = "Mindkettő elem";
+            this.Dolgozikmind2.UseVisualStyleBackColor = true;
+            this.Dolgozikmind2.CheckedChanged += new System.EventHandler(this.Dolgozikmind2_CheckedChanged);
+            // 
+            // Dolgozikment
+            // 
+            this.Dolgozikment.Location = new System.Drawing.Point(337, 287);
+            this.Dolgozikment.Name = "Dolgozikment";
+            this.Dolgozikment.Size = new System.Drawing.Size(75, 23);
+            this.Dolgozikment.TabIndex = 7;
+            this.Dolgozikment.Text = "Ment";
+            this.Dolgozikment.UseVisualStyleBackColor = true;
+            this.Dolgozikment.Click += new System.EventHandler(this.Dolgozikment_Click);
+            // 
+            // Dolgozok
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 355);
+            this.Controls.Add(this.Dolgozikment);
+            this.Controls.Add(this.Dolgozikmind2);
+            this.Controls.Add(this.Dolgozokadatok);
+            this.Controls.Add(this.Dolgozokosszesito);
+            this.Controls.Add(this.Dolgozokfiu2);
+            this.Controls.Add(this.Dolgozoklany2);
+            this.Controls.Add(this.Dolgozokfiu1);
+            this.Controls.Add(this.Dolgozoklany1);
+            this.Name = "Dolgozok";
+            this.Text = "Dolgozok";
+            this.Dolgozokosszesito.ResumeLayout(false);
+            this.Dolgozokosszesito.PerformLayout();
+            this.Dolgozokadatok.ResumeLayout(false);
+            this.Dolgozokadatok.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Dolgozoklany1;
+        private System.Windows.Forms.Label Dolgozokfiu1;
+        private System.Windows.Forms.ComboBox Dolgozoklany2;
+        private System.Windows.Forms.ComboBox Dolgozokfiu2;
+        private System.Windows.Forms.GroupBox Dolgozokosszesito;
+        private System.Windows.Forms.Label Osszesito6ev1;
+        private System.Windows.Forms.Label Osszesitokor1;
+        private System.Windows.Forms.Label Osszesitolegidosebb1;
+        private System.Windows.Forms.RadioButton Osszesitofiu;
+        private System.Windows.Forms.RadioButton Osszesitolany;
+        private System.Windows.Forms.GroupBox Dolgozokadatok;
+        private System.Windows.Forms.TextBox Osszesito6ev2;
+        private System.Windows.Forms.TextBox Osszesitokor2;
+        private System.Windows.Forms.TextBox Osszesitolegidosebb2;
+        private System.Windows.Forms.TextBox Adatokkor2;
+        private System.Windows.Forms.TextBox Adatokmiota2;
+        private System.Windows.Forms.Label Adatokmiota1;
+        private System.Windows.Forms.Label Adatokkor1;
+        private System.Windows.Forms.CheckBox Dolgozikmind2;
+        private System.Windows.Forms.Button Dolgozikment;
+    }
+}
+
---
+++ WindowsFormsApplication7Modulzaro/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication7Modulzaro
+{
+    public partial class Dolgozok : Form
+    {
+        string[] beolvas = File.ReadAllLines("emberek.txt", Encoding.Default);
+        List<int> dolgozikevl = new List<int>();
+        List<int> dolgozikevf = new List<int>();
+        List<int> koral = new List<int>();
+        List<int> koraf = new List<int>();
+        List<int> dolgozikev = new List<int>();
+        public Dolgozok()
+        {
+            InitializeComponent();
+            for (int i = 1; i < beolvas.Length; i++)
+            {
+                if (beolvas[i].Split(';')[2] == "L")
+                {
+                    Dolgozoklany2.Items.Add(beolvas[i].Split(';')[0]);
+                    koral.Add(int.Parse(beolvas[i].Split(';')[1]));
+                    if ((beolvas[i].Contains("L;")))
+                    {
+                        dolgozikevl.Add(int.Parse(beolvas[i].Split(';')[3]));
+                        dolgozikev.Add(int.Parse(beolvas[i].Split(';')[3]));
+                    }
+
+                }
+                else
+                {
+                    Dolgozokfiu2.Items.Add(beolvas[i].Split(';')[0]);
+                    koraf.Add(int.Parse(beolvas[i].Split(';')[1]));
+                    if (beolvas[i].Contains("F;"))
+                    {
+                        dolgozikevf.Add(int.Parse(beolvas[i].Split(';')[3]));
+                        dolgozikev.Add(int.Parse(beolvas[i].Split(';')[3]));
+                    }
+                }
+
+            }
+        }
+
+        private void Dolgozoklany2_SelectedValueChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < beolvas.Length; i++)
+            {
+                if (beolvas[i].Contains(Dolgozoklany2.SelectedItem.ToString()))
+                {
+                    Adatokkor2.Text = beolvas[i].Split(';')[1];
+                    Adatokmiota2.Text = "";
+                    if ((beolvas[i].Contains("L;")))
+                    {
+                        Adatokmiota2.Text = beolvas[i].Split(';')[3];
+                    }
+                }
+            }
+        }
+
+        private void Dolgozokfiu2_SelectedValueChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < beolvas.Length; i++)
+            {
+                if (beolvas[i].Contains(Dolgozokfiu2.SelectedItem.ToString()))
+                {
+                    Adatokkor2.Text = beolvas[i].Split(';')[1];
+                    Adatokmiota2.Text = "";
+                    if ((beolvas[i].Contains("F;")))
+                    {
+                        Adatokmiota2.Text = beolvas[i].Split(';')[3];
+                    }
+                }
+
+            }
+        }
+
+        private void Osszesitolany_CheckedChanged(object sender, EventArgs e)
+        {
+            Osszesitolegidosebb2.Text = max(koral) + "";
+            Osszesitokor2.Text = osszkor(koral) + "";
+            Osszesito6ev2.Text = hateveki(dolgozikevl, "L;") + "";
+        }
+
+        private void Osszesitofiu_CheckedChanged(object sender, EventArgs e)
+        {
+            Osszesitolegidosebb2.Text = max(koraf) + "";
+            Osszesitokor2.Text = osszkor(koraf) + "";
+            Osszesito6ev2.Text = hateveki(dolgozikevf, "F;") + "";
+        }
+        private void Dolgozikmind2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (max(koral) > max(koraf))
+            {
+                Osszesitolegidosebb2.Text = max(koral) + "";
+            }
+            else
+            {
+                Osszesitolegidosebb2.Text = max(koraf) + "";
+            }    
+            Osszesitokor2.Text = osszkor(koral) + osszkor(koraf) + "";
+            if (hateveki(dolgozikevf, "F;") == "nincs" && hateveki(dolgozikevl, "L;") == "nincs")
+            {
+                Osszesito6ev2.Text = "nincs";
+            }
+            else if (hateveki(dolgozikevf, "F;") == "nincs")
+            {
+                Osszesito6ev2.Text = hateveki(dolgozikevl, "L;") + "";
+            }
+            else if (hateveki(dolgozikevl, "L;") == "nincs")
+            {
+                Osszesito6ev2.Text = hateveki(dolgozikevf, "F;") + "";
+            }
+            else
+            {
+                Osszesito6ev2.Text = hateveki(dolgozikevf, "F;") + ", " + hateveki(dolgozikevl, "L;");
+            }
+          
+        }
+
+        private void Dolgozikment_Click(object sender, EventArgs e)
+        {
+            string kiir = "";
+            if (Osszesitolany.Checked)
+            {
+                kiir = kiir + "Lányok: " + Environment.NewLine + "Legidősebb: " + Osszesitolegidosebb2.Text + Environment.NewLine + "Összes kor: " + Osszesitokor2.Text + Environment.NewLine + "6 éve dolgozó: " + Osszesito6ev2.Text + Environment.NewLine;
+                File.AppendAllText("dolgozok.txt", kiir);
+            }
+            else if (Osszesitofiu.Checked)
+            {
+                kiir = kiir + "Fiúk: " + Environment.NewLine + "Legidősebb: " + Osszesitolegidosebb2.Text + Environment.NewLine + "Összes kor: " + Osszesitokor2.Text + Environment.NewLine + "6 éve dolgozó: " + Osszesito6ev2.Text + Environment.NewLine;
+                File.AppendAllText("dolgozok.txt", kiir);
+            }
+        }
+        public int max(List<int> koraf)
+        {
+            int maxi = koraf[0];
+            for (int i = 0; i < koraf.Count; i++)
+            {
+                if (koraf[i] > koraf[0])
+                {
+                    maxi = koraf[i];
+                }
+            }
+            return maxi;
+        }
+        public int osszkor(List<int> koraf)
+        {
+            int ossz = 0;
+            for (int i = 0; i < koraf.Count; i++)
+            {
+                ossz = ossz + koraf[i];
+            }
+            return ossz;
+        }
+        public int hateve(List<int> ev)
+        {
+            int i = 0;
+            while (i < ev.Count && !(ev[i] > 6))
+            {
+                i++;
+            }
+            if (i < ev.Count)
+            {
+                return ev[i];
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public string hateveki(List<int> ev, string nem)
+        {
+            string a = "";
+            if (hateve(ev) > 6)
+            {
+                for (int i = 0; i < beolvas.Length; i++)
+                {
+                    if ((beolvas[i].Contains(nem)))
+                    {
+                        if (int.Parse(beolvas[i].Split(';')[3]) == hateve(ev))
+
+                        {
+                            a = beolvas[i].Split(';')[0];
+                        }
+                    }
+                }
+            }
+            else
+            {
+                a = "nincs";
+            }
+            return a;
+        }
+
+        private void Dolgozoklany2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
